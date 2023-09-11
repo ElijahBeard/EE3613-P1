29ccc6f267162c1969afc9dd993b942cb8589d0e
ElijahBeard <elijahbeard24@gmail.com>
2023-09-11 11:43:16 -0400

    fin

--- source/main.cpp
+++ source/main.cpp
@@ -4,4 +4,25 @@ using namespace std;
 
 int main(int argc, char** argv) {
   LinkedList<int> list;
+  char choice = '-';
+  int data;
+  do{
+    cin >> choice;
+    if(choice == 'p'){
+      list.print();
+    }
+    else if(choice == 'a'){
+      cin >> data;
+      list.push_front(data);
+    }
+    else if(choice == 'd'){
+      list.pop_front();
+    }
+    else if(choice == 'r'){
+      list.reverse();
+    }
+    else{
+      ;
+    }
+  }while(choice != 'q');
 }
