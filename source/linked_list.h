bcf30c054c1370dde1e0bd69185744185c629eb7
ElijahBeard <elijahbeard24@gmail.com>
2023-09-11 11:43:05 -0400

    fin

--- source/linked_list.h
+++ source/linked_list.h
@@ -1,20 +1,17 @@
-
 #include <iostream>
 using namespace std;
-
 template <typename T>
 class LinkedList {
-
 public:
   LinkedList() : head(NULL) {}
 
   // You need to implement the following functions. 
   // 
-  // ~LinkedList();
-  // void push_front(const T data);
-  // void pop_front();
-  // void reverse();
-  // void print() const;
+  ~LinkedList();
+  void push_front(const T data);
+  void pop_front();
+  void reverse();
+  void print() const;
 
 private:
   
@@ -29,3 +26,80 @@ private:
 };
 
 
+template <typename T>
+LinkedList<T>::~LinkedList(){
+  if( head != NULL ){
+    ListNode* target = head;
+    ListNode* prev = head;
+    while( target->next != NULL ){
+      prev = target;
+      target = target->next;
+      delete prev;
+      prev = NULL;
+    }
+  }
+}
+
+template <typename T>
+void LinkedList<T>::pop_front(){
+  if( head == NULL ){
+    return;
+  }
+  if( head->next == NULL ){
+    delete head;
+    head = NULL;
+  }
+  if( head != NULL ){
+    ListNode* prev = head;
+    ListNode* target = head->next;
+    while(target->next != NULL){
+      prev = prev->next;
+      target = target->next;
+    }
+    prev->next = NULL;
+    delete target;
+  }
+}
+
+template <typename T>
+void LinkedList<T>::print() const{
+  if( head != NULL ){
+    ListNode* target = head;
+    cout << target->data;
+    while(target->next != NULL){
+      target = target->next;
+      cout << target->data;
+    }
+    return;
+  }
+}
+
+template <typename T>
+void LinkedList<T>::reverse(){
+    ListNode* prev = NULL;
+    ListNode* current = head;
+    ListNode* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    
+    head = prev;
+}
+
+template <typename T>
+void LinkedList<T>::push_front(const T data){
+  if( head == NULL ){
+    head = new ListNode(data);
+  }
+  else{
+    ListNode* target = head;
+    while(target->next != NULL){
+      target = target->next;
+    }
+    target->next = new ListNode(data);
+  }
+}
